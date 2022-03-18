be857f32c525649812c00b7d88d4e24a98c5ea6d
Dmitriy <dmitriyy311@gmail.com>
2022-03-18 21:17:21 +0200

    added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
 import HomePage from './pages/Home';
 
@@ -7,6 +7,11 @@ import ProjectsIcon from './images/projects.png';
 import BooksIcon from './images/books.png';
 import FormIcon from './images/form.png';
 
+import HomeIconActive from './images/home-w.png';
+import ProjectsIconActive from './images/projects-w.png';
+import BooksIconActive from './images/books-w.png';
+import FormIconActive from './images/form-w.png';
+
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
 import ContactsPage from "./pages/Contacts";
@@ -14,26 +19,30 @@ import './sass/index.sass';
 
 function App() {
   const data = [
-    {default: HomeIcon, active: 'home-w.png', link: '/', id: 0},
-    {default: ProjectsIcon, active: 'projects-w.png', link: '/projects', id: 1},
-    {default: BooksIcon, active: 'books-w.png', link: '/experience', id: 2},
-    {default: FormIcon, active: 'form-w.png', link: '/contacts', id: 3}
+    {default: HomeIcon, active: HomeIconActive, link: '/', id: 0},
+    {default: ProjectsIcon, active: ProjectsIconActive, link: '/projects', id: 1},
+    {default: BooksIcon, active: BooksIconActive, link: '/experience', id: 2},
+    {default: FormIcon, active: FormIconActive, link: '/contacts', id: 3}
   ];
 
   return (
     <Fragment>
       <div className="container-wrap">
         <div className="panel">
-          <div className=".panel__center">
+          <div className="panel__center">
             {
               data.map(item => {
                 return (
                   <NavLink to={item.link} key={item.id} className={(navData) => {
                     return navData.isActive ? 'panel__center-item pannel__center-active' : 'panel__center-item'
                   }}>
-                    <div>
-                      <img src={item.default} alt="navigation" />
-                    </div>
+                    {
+                      ({ isActive }) => (
+                        <div>
+                          <img src={isActive ? item.active : item.default} alt="navigation" />
+                        </div>
+                      )
+                    }
                   </NavLink>
                 )
               })
